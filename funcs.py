2a78ac469ccb6c2205932ea8e5fa0ba7b0b0d745
Aleksei Abramov <109025285+WeinsGH@users.noreply.github.com>
2023-12-04 23:12:15 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import torch
-import requests
 from io import BytesIO
-import pandas as pd
 from torchvision import transforms as T
 
 def preprocess_image(image):
@@ -18,23 +16,3 @@ def preprocess_image(image):
 def transforming(image):
     transform = T.Compose([T.Resize((440, 440))])
     return transform(image)
-
-
-def detect_objects(image, model):
-    # Преобразование изображения, чтобы соответствовать ожиданиям модели
-    transform = T.Compose([
-        T.Resize((640, 640)),  # Размер может отличаться в зависимости от вашей модели
-        T.ToTensor(),
-        # Другие преобразования, которые могут потребоваться
-    ])
-
-    input_image = transform(image).unsqueeze(0)  # Добавляем размерность батча
-
-    # Переводим модель в режим оценки и применяем предсказание
-    model.eval()
-    with torch.no_grad():
-        predictions = model(input_image)
-
-
-
-    return predictions
